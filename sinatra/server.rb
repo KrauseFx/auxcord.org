c5cec4868f0fc0ac8c96393ad361f7e3c61acaba
Felix Krause <krausefx@gmail.com>
2022-03-22 17:46:26 +0100

    Update sinatra flow

--- sinatra/server.rb
+++ sinatra/server.rb
@@ -7,14 +7,21 @@ enable :sessions
 # set :session_store, Rack::Session::Pool
 
 get "/" do
+  session[:user_id] = 7 # TODO: remove
   # redirect_uri = request.scheme + "://" + request.host + (request.port == 4567 ? ":#{request.port}" : "") + "/sonos/authorized.html"
-  redirect_uri = "http://localhost:4567/sonos/authorized.html"
-  @sonos_login_url = "https://api.sonos.com/login/v3/oauth?" +
-                  "client_id=#{ENV.fetch('SONOS_KEY')}&" + 
-                  "response_type=code&" + 
-                  "state=TESTSTATE&" + 
-                  "scope=playback-control-all&" + 
-                  "redirect_uri=#{ERB::Util.url_encode(redirect_uri)}"
+
+  if Db.sonos_tokens.where(user_id: session[:user_id]).count == 0
+    redirect_uri = "http://localhost:4567/sonos/authorized.html"
+    @sonos_login_url = "https://api.sonos.com/login/v3/oauth?" +
+                    "client_id=#{ENV.fetch('SONOS_KEY')}&" + 
+                    "response_type=code&" + 
+                    "state=TESTSTATE&" + 
+                    "scope=playback-control-all&" + 
+                    "redirect_uri=#{ERB::Util.url_encode(redirect_uri)}"
+  else
+    # TODO: Generate Spotify URL here
+    binding.pry
+  end
   erb :login
 end
 
@@ -28,8 +35,14 @@ get "/sonos/authorized.html" do
 
   # Now process the Sonos login
   authorization_code = params.fetch(:code)
-  sonos = SonosPartyMode::Sonos.new(
+  SonosPartyMode::Sonos.new(
     authorization_code: authorization_code,
     user_id: user_id
   )
+
+  redirect_to :root
+end
+
+get "spotify/authorized.html" do
+
 end
